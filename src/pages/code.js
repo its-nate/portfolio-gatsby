af229bddebc9f7b93f2a7612dfa84af3d98fdc8f
Nate Micinski <nmicinski@gmail.com>
2020-06-26 18:30:35 -0700

    add code page content

--- src/pages/code.js
+++ src/pages/code.js
@@ -5,7 +5,46 @@ import SEO from "../components/seo"
 const Code = () => (
   <Layout>
     <SEO title="code" />
-    <h1>code</h1>
+    <div className="container">
+      <div className="row">
+        <div className="col s12">
+          <h1 className="page-header">It's my code.</h1>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col s12">
+          <p className="page-body mb-0">
+            Feel free to check out my{" "}
+            <a
+              href="https://github.com/DearLorditsNate"
+              target="_blank"
+              rel="noreferrer"
+              class="page-link"
+            >
+              GitHub
+            </a>
+            .
+          </p>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col s12">
+          <p className="page-body">
+            I also profile some projects I’ve completed through my
+            company,&nbsp;
+            <a
+              href="https://www.holisticwebsolutions.com"
+              target="_blank"
+              rel="noreferrer"
+              class="page-link"
+            >
+              Holistic Web Solutions
+            </a>
+            , on their site.
+          </p>
+        </div>
+      </div>
+    </div>
   </Layout>
 )
 
