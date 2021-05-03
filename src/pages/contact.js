c283e6ca370e4e543940126ac67e50c2657f42aa
Nate Micinski <nmicinski@gmail.com>
2021-05-03 12:59:10 -0700

    update mail

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -20,10 +20,10 @@ const Contact = () => (
         <div className="col s12">
           <p className="page-body mb-0">
             <a
-              href="mailto:nmicinski@gmail.com"
+              href="mailto:nate@itsnate.net"
               className="page-link"
             >
-              nmicinski@gmail.com
+              nate@itsnate.net
             </a>
           </p>
         </div>
